c87c7b206994538d406956388f6f5d04d6470283
Steudnera Taro <wowcxy2008@126.com>
2018-05-25 11:39:34 +0800

    Create UserServiceImpl.java

---
+++ src/main/java/cn/steudnera/service/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package cn.steudnera.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.steudnera.dao.UserDao;
+import cn.steudnera.model.User;
+import cn.steudnera.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+	@Autowired  
+  private UserDao userDao;  
+
+  public User selectUserById(Integer userId) {  
+    return userDao.selectUserById(userId);
+  } 
+}
