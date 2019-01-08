c679b4cf67809fa1aafd73cee2cfc4d54820330c
g <x@y.com>
2019-01-08 10:01:31 +0800

    bug#fix

--- src/main/java/cn/steudnera/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package cn.steudnera.dao;
-
-import cn.steudnera.model.User;
-
-public interface UserDao {
-	public User selectUserById(Integer userId);  
-}
--- src/main/java/cn/steudnera/service/UserService.java
+++
@@ -1,6 +0,0 @@
-package cn.steudnera.service;
-import cn.steudnera.model.User;
-
-public interface UserService {
-	User selectUserById(Integer userId);
-}
