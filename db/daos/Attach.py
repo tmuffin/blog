227436f1d0496a8d7e821049e28bf108d965e636
chenxiangyu <wowcxy2008@126.com>
2019-04-15 15:38:15 +0800

    bug#fix

--- db/daos/Attach.py
+++ db/daos/Attach.py
@@ -3,12 +3,21 @@
     @author Philip
 '''
 from django.db import models
-from . import File
+from . import File, Event, Project, Diary
 
 # 内容
 class Attach(models.Model):
-    # 文件
-    file = models.ForeignKey(File, on_delete = models.SET_NULL, null = False)
+    # 事件
+    file = models.ForeignKey(File, on_delete = models.SET_NULL, null = True)
+
+    # 日志
+    diary = models.ForeignKey(Diary, on_delete = models.SET_NULL, null = True)
+
+    # 内容
+    event = models.ForeignKey(Event, on_delete = models.SET_NULL, null = True)
+
+    # 项目
+    project = models.ForeignKey(Project, on_delete = models.SET_NULL, null = True)
 
     # 保存日期
     createdAt = models.DateTimeField(auto_now = True)
